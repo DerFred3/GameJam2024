5f1b3b82dd8cb2f49acefb34ab21ced7e309ca07
Peter Zdankin <peter.zdankin@gmail.com>
2024-07-06 00:55:25 +0200

    penis

---
+++ Kuckucks Uhrwerk/Assets/GlobalVariables.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVariables : MonoBehaviour
+{
+    
+    public static float globalRotation = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate(){
+        if(globalRotation >= 90.0f){
+            globalRotation = -90.0f;
+        }
+        globalRotation += 1f;
+       
+    }
+}
