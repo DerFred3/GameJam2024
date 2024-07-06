83dd659feb372771d82d4eb88503503f2ac90ca5
Peter Zdankin <peter.zdankin@gmail.com>
2024-07-06 12:52:12 +0200

    level 1

--- Kuckucks Uhrwerk/Assets/Scripts/GlobalVariables.cs
+++ Kuckucks Uhrwerk/Assets/Scripts/GlobalVariables.cs
@@ -7,10 +7,11 @@ public class GlobalVariables : MonoBehaviour
     
     public float globalRotation = 0.0f;
     public float globalRotationScale = 10.0f;
+    private CrankHandle crankHandle;
     // Start is called before the first frame update
     void Start()
     {
-        
+        crankHandle = GameObject.Find("Crank").GetComponent<CrankHandle>();
     }
 
     // Update is called once per frame
@@ -19,6 +20,7 @@ public class GlobalVariables : MonoBehaviour
         
     }
     void FixedUpdate(){
+        globalRotation = crankHandle.GetCurrentValue(-10, 10);
         //if(globalRotation >= 90.0f){
         //    globalRotation = -90.0f;
         //}
