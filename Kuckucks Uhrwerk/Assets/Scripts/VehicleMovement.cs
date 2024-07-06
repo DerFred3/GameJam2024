b3a974ade4bcddcf7152914d57b46e4eb93f05f5
Lars <Lars.Gilhaus@stud.uni-due.de>
2024-07-06 14:06:35 +0200

    Add vehicle for player movement with preparations for boost platforms

--- Kuckucks Uhrwerk/Assets/Scripts/VehicleMovement.cs
+++ Kuckucks Uhrwerk/Assets/Scripts/VehicleMovement.cs
@@ -5,34 +5,39 @@ using UnityEngine;
 public class VehicleMovement : MonoBehaviour
 {
     [SerializeField] private float speedMultiplier;
+    [SerializeField] private float boostMultiplier;
+
+    private float defaultSpeed;
 
     private Rigidbody2D rb;
     private List<GameObject> currentlyTouching = new List<GameObject>();
 
     private Vector3 movementDirection;
 
-    private bool isGrounded;
-
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        defaultSpeed = speedMultiplier;
     }
 
     private void Update()
     {
         if (currentlyTouching.Count <= 0) return;
-
-        transform.position += movementDirection * speedMultiplier * Time.deltaTime;
+        rb.velocity = movementDirection * speedMultiplier * Time.deltaTime;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        movementDirection = collision.transform.right;
+        currentlyTouching.Add(collision.gameObject);
+
         switch (collision.gameObject.tag)
         {
             case "Ground":
-                currentlyTouching.Add(collision.gameObject);
-                movementDirection = collision.transform.right.normalized;
-                rb.isKinematic = true;
+                speedMultiplier = defaultSpeed;
+                break;
+            case "Speed":
+                speedMultiplier *= boostMultiplier;
                 break;
             default:
                 Debug.Log("Collided with object with tag: `" + collision.gameObject.tag + "` for which no interaction is specified!");
@@ -42,11 +47,12 @@ public class VehicleMovement : MonoBehaviour
 
     private void OnCollisionExit2D(Collision2D collision)
     {
+        currentlyTouching.Remove(collision.gameObject);
         switch (collision.gameObject.tag)
         {
             case "Ground":
-                currentlyTouching.Remove(collision.gameObject);
-                rb.isKinematic = false;
+                break;
+            case "Speed":
                 break;
             default:
                 Debug.Log("Collision with object with tag: `" + collision.gameObject.tag + "` for which no interaction is specified!");
