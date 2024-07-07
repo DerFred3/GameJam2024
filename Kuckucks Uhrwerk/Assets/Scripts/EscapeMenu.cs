18b871467e57125ee239a9e742a0cc623f184640
Lars <Lars.Gilhaus@stud.uni-due.de>
2024-07-07 15:12:35 +0200

    Add escape menu prefab; Add pivot point reference to global pivoting script for cranks

---
+++ Kuckucks Uhrwerk/Assets/Scripts/EscapeMenu.cs
@@ -0,0 +1,78 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EscapeMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject[] toActivateGameObjects;
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button restartButton;
+    [SerializeField] private Button quitButton;
+
+    private bool menuActive = false;
+
+    private void Start()
+    {
+        DeactivateEscapeMenu();
+
+        resumeButton.onClick.AddListener(ResumeButton_OnClick);
+        restartButton.onClick.AddListener(RestartButton_OnClick);
+        quitButton.onClick.AddListener(QuitButton_OnClick);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (menuActive)
+            {
+                DeactivateEscapeMenu();
+            } 
+            else
+            {
+                ActivateEscapeMenu();
+            }
+        }
+    }
+
+    public void ActivateEscapeMenu()
+    {
+        Time.timeScale = 0f;
+
+        foreach (GameObject gameObject in toActivateGameObjects)
+        {
+            gameObject.SetActive(true);
+        }
+        menuActive = true;
+    }
+
+    public void DeactivateEscapeMenu()
+    {
+        Time.timeScale = 1f;
+
+        foreach (GameObject gameObject in toActivateGameObjects)
+        {
+            gameObject.SetActive(false);
+        }
+        menuActive = false;
+    }
+
+    private void ResumeButton_OnClick()
+    {
+        DeactivateEscapeMenu();
+    }
+
+    private void RestartButton_OnClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void QuitButton_OnClick()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#endif
+    }
+}
