f253c16fb60b97a665b2bf06a3c50e894e20f5ad
TillJWalter <tillusjonas@gmail.com>
2024-07-07 16:38:12 +0200

    kuckuck sound

--- Kuckucks Uhrwerk/Assets/Scripts/RotateTile.cs
+++ Kuckucks Uhrwerk/Assets/Scripts/RotateTile.cs
@@ -15,6 +15,6 @@ public class RotateTile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.eulerAngles = new Vector3(0,0, globalVariables.globalRotationScale*globalVariables.globalRotation+rotationAngle);
+        gameObject.transform.eulerAngles = new Vector3(0,0, -globalVariables.globalRotationScale*globalVariables.globalRotation+rotationAngle);
     }
 }
